6d47032c007190ff02f916f84e4ae96a07898fa5
Salih <Salih@DESKTOP-9G48RKD>
2021-01-27 22:27:01 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+        public MyDictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+        public void Add(Tkey key,Tvalue value)
+        {
+            Tkey[] tempKeys = keys;
+            Tvalue[] tempValues = values;
+
+            keys = new Tkey[keys.Length + 1];
+            values = new Tvalue[values.Length + 1];
+            
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = value;
+
+        }
+        public void ListShow()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " = " + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> isim = new MyDictionary<int, string>();
+            isim.Add(9,"Salih Gül");
+            isim.Add(4, "Ali Candan");
+            isim.Add(6, "Polat Alemdar");
+            isim.Add(75, "Kadir Alıcı");
+            isim.Add(786, "Rüştü Gelir");
+            isim.Add(52, "Engin Demiroğ");
+            isim.Add(31, "Hakan Arıcı");
+
+
+            isim.ListShow();
+
+            Console.ReadKey();
+        }
+    }
+}
